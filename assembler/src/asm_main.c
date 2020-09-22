8a620ca3d8beff84ce340ad2d9e52208b60d9d73
AndreIglesias <ciglesia@student.42.fr>
2020-09-22 13:02:54 +0200

    asm_main:bg

--- assembler/src/asm_main.c
+++ assembler/src/asm_main.c
@@ -6,7 +6,7 @@
 /*   By: ciglesia <ciglesia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/05 22:21:40 by ciglesia          #+#    #+#             */
-/*   Updated: 2020/09/20 20:38:02 by ciglesia         ###   ########.fr       */
+/*   Updated: 2020/09/22 13:01:35 by ciglesia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int		main(int ac, char **av)
 {
 	t_file	file;
 
-	if (ac != 2 || (ac == 3 && ft_strcmp(av[1], "-v") != 0))
+	if (ac != 2 && (ac == 3 && ft_strcmp(av[1], "-v") != 0))
 		return (ft_puterr(ERROR""RED": assembler needs exactly one file!"E0M,
 						EXIT_FAILURE));
 	else if (ac == 2 || (ac == 3 && ft_strcmp(av[1], "-v") == 0))
